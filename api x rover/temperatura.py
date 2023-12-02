534add7831ff917dda7007e6c3951faf0cb88e0e
mike200326 <117249589+mike200326@users.noreply.github.com>
2023-12-01 21:43:24 -0600

    Add files via upload

---
+++ api x rover/temperatura.py
@@ -0,0 +1,17 @@
+class Temperatura:
+    def __init__(self,id,temperatura,fecha):
+        self.id=id
+        self.temperatura=temperatura
+        self.fecha=fecha
+
+class Distancia:
+    def __init__(self, id, distancia, fecha):
+        self.id = id
+        self.distancia = distancia
+        self.fecha = fecha
+
+class Presion:
+    def __init__(self, id, presion, fecha):
+        self.id = id
+        self.presion = presion
+        self.fecha = fecha
